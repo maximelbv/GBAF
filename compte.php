932ab1b0ed9c6e81eea6e71a815e995acad196e5
Maxime Lefebvre <lefebvremaxime00@gmail.com>
2021-04-19 09:37:44 +0200

    account page : added the redirection when the user change his informations, also implemented the error when the username is already taken

--- compte.php
+++ compte.php
@@ -1,6 +1,7 @@
 <?php 
     session_start(); 
     include('include/_bdd_call.php');
+    $liste = $bdd->query('SELECT username FROM account WHERE username="'.$_POST["nv_username"].'"')->fetchAll();
 ?>
 
 <!DOCTYPE html>
@@ -28,22 +29,33 @@
                         if (!empty($_POST['nv_prenom']) && isset($_POST['nv_prenom'])) {
                             $change = $bdd->prepare('UPDATE account SET prenom = ? WHERE id_user = ?');
                             $change->execute([$_POST['nv_prenom'], $_SESSION['id_user']]);
+                            header('Location: redirection_changement_infos.php');
                         }
                         if (!empty($_POST['nv_nom']) && isset($_POST['nv_nom'])) {
                             $change = $bdd->prepare('UPDATE account SET nom = ? WHERE id_user = ?');
                             $change->execute([$_POST['nv_nom'], $_SESSION['id_user']]);
+                            header('Location: redirection_changement_infos.php');
                         }
-                        if (!empty($_POST['nv_username']) && isset($_POST['nv_username'])) {
-                            $change = $bdd->prepare('UPDATE account SET username = ? WHERE id_user = ?');
-                            $change->execute([$_POST['nv_username'], $_SESSION['id_user']]);
-                        }
+                        if (!empty($_POST['nv_username']) && isset($_POST['nv_username']) && count($liste) == 0) {
+                                
+                                $change = $bdd->prepare('UPDATE account SET username = ? WHERE id_user = ?');
+                                $change->execute([$_POST['nv_username'], $_SESSION['id_user']]);
+                                header('Location: redirection_changement_infos.php');
+                            } else if (!empty($_POST['nv_username']) && isset($_POST['nv_username'])) {
+                                echo "identifiant déjà utilisé";
+                            }
+                            
+                        
                         if (!empty($_POST['nv_password']) && isset($_POST['nv_password'])) {
                             $change = $bdd->prepare('UPDATE account SET password = ? WHERE id_user = ?');
                             $pass_hash = password_hash($_POST['nv_password'], PASSWORD_DEFAULT);
                             $change->execute([$pass_hash, $_SESSION['id_user']]);
+                            header('Location: redirection_changement_infos.php');
                         }
+                        
                 ?>
-
+                        
+                        <pre><?php print_r(count($liste)); ?></pre>
             </fieldset>
 
         </form>
---
+++ redirection_changement_infos.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<?php include('include/_head.php') ?>
+
+<body class="body_redirection">
+    <img src="media/check.png" alt="" width="100px">
+    <h1>Succès !</h1>
+    <h2>Vos informations ont bien été modifiées <br>Veuillez vous reconnecter.</h2>
+    <?php
+        header("Refresh:3; url=http://localhost/Formation/GBAF/connexion.php");
+    ?>
+</body>
+</html>
