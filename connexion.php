4ddf4fb74164713e22d3bccbd2e0f116992ee61f
Maxime Lefebvre <lefebvremaxime00@gmail.com>
2021-04-26 11:01:58 +0200

    commented the login / register pages

--- connexion.php
+++ connexion.php
@@ -1,28 +1,28 @@
  <?php 
 
-    include('./include/_bdd_call.php');
+    include('./include/_bdd_call.php'); // appel à la bdd 
 
     if (isset($_POST['pseudo'],$_POST['mdp'])) { 
 
         $reponse = $bdd->query('SELECT * FROM account WHERE username="'.$_POST["pseudo"].'"')->fetchAll(); 
 
-        if (array_key_exists(0, $reponse)) {
+        if (array_key_exists(0, $reponse)) { // si le pseudo existe dans la bdd
 
-            $donnees = $reponse[0];
-            $isPasswordCorrect = password_verify($_POST['mdp'], $donnees['password']);  
+            $donnees = $reponse[0]; // stock ses valeurs dans $donnees
+            $isPasswordCorrect = password_verify($_POST['mdp'], $donnees['password']); // vérifie le mot de passe
 
-            if ($_POST['pseudo'] == $donnees['username'] && $isPasswordCorrect) { 
+            if ($_POST['pseudo'] == $donnees['username'] && $isPasswordCorrect) { // si l'username et le mdp sont corrects
 
-                session_start();                                                                   
-                $_SESSION['username'] = $donnees['username'];   
+                session_start();                                                  // débute la session                 
+                $_SESSION['username'] = $donnees['username'];                     // définit les variables de session
                 $_SESSION['prenom'] = $donnees['prenom'];
                 $_SESSION['nom'] = $donnees['nom']; 
                 $_SESSION['id_user'] = $donnees['id_user'];      
                 $_SESSION['question'] = $donnees['question'];
                 $_SESSION['reponse'] = $donnees['reponse'];                                       
-                header('LOCATION: index.php');                                                                
-            } else { 
-                $mauvaisid =  "mauvais identifiant ou mot de passe"; 
+                header('LOCATION: index.php');                                    // va à l'index                        
+            } else {                                                              // sinon
+                $mauvaisid =  "mauvais identifiant ou mot de passe";              // donne la valeur de $mauvaisid
             }
         } else { 
             $mauvaisid =  "mauvais identifiant ou mot de passe"; 
@@ -55,8 +55,8 @@
                     <div><input type="submit" value="Se connecter"></div>   
                     <div class="log_form__error_reverse">                                                                                                     
                         <?php 
-                            if (isset($mauvaisid)) {
-                                echo $mauvaisid ?> <img src="media/error.png" alt="" width="15" height="15"> <?php
+                            if (isset($mauvaisid)) {    // si $mauvaisid est définie
+                                echo $mauvaisid ?> <img src="media/error.png" alt="" width="15" height="15"> <?php  // display $mauvaisid
                             }     
                         ?>
                     </div> 
--- inscription.php
+++ inscription.php
@@ -7,8 +7,10 @@
     </head>
 
     <body class="log_body">
-        
-            <form  action="inscription.php" method="post">
+
+                <!-- Formulaire d'inscription -->
+
+            <form  action="inscription.php" method="post"> 
 
                 <fieldset class="log_form">
 
@@ -35,20 +37,20 @@
                     
                     <?php
 
-                    include('./include/_bdd_call.php');
+                    include('./include/_bdd_call.php'); // appel à la bdd
 
-                        if (isset($_POST['nom']) && !empty($_POST['nom'])
+                        if (isset($_POST['nom']) && !empty($_POST['nom']) // si tout les champs sont remplis et non vides
                         && isset($_POST['prenom']) && !empty($_POST['prenom']) 
                         && isset($_POST['username']) && !empty($_POST['username']) 
                         && isset($_POST['password']) &&!empty($_POST['password']) 
                         && isset($_POST['reponse_secrete']) && !empty($_POST['reponse_secrete'])) {
 
-                            $reponse = $bdd->query('SELECT * FROM account WHERE username="'.$_POST["username"].'"')->fetchAll(); 
-                            if (count($reponse) == 0) {
-                                    $pass_hash = password_hash($_POST['password'], PASSWORD_DEFAULT);                                                                                 // hash le password  
-                                    $reponse = $bdd->prepare('INSERT INTO account(nom,prenom,username,password,question,reponse) VALUES(?,?,?,?,?,?)');          // va chercher les données de la bdd et demande leur valeur via $reponse
+                            $reponse = $bdd->query('SELECT * FROM account WHERE username="'.$_POST["username"].'"')->fetchAll(); // stock dans $reponse les entrées de account quand l'username est égal a l'username entré
+                            if (count($reponse) == 0) { // si l'identifiant n'existe pas dans la base de données
+                                    $pass_hash = password_hash($_POST['password'], PASSWORD_DEFAULT); // hash le password  
+                                    $reponse = $bdd->prepare('INSERT INTO account(nom,prenom,username,password,question,reponse) VALUES(?,?,?,?,?,?)'); // insère une nouvelle entrée dans la bdd
                                     $reponse->execute(array($_POST['nom'], $_POST['prenom'], $_POST['username'], $pass_hash, $_POST['question_secrete'], $_POST['reponse_secrete'])); // execute les entrées du formulaire en tant que valeurs pour les données demandées dans $reponse et inclut les dans la bdd   
-                                    header('Location: redirection_nv_compte.php');      
+                                    header('Location: redirection_nv_compte.php'); // redirection 
                             } else { ?>   
                                 <div class="log_form__error">
                                     <img src="media/error.png" alt="" width="15" height="15"> 
