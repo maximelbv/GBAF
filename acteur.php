b2b6f87682649d6e731e0f880aee03f083411bc6
Maxime Lefebvre <lefebvremaxime00@gmail.com>
2021-04-26 12:05:57 +0200

    commented a page that i forgot to comment

--- acteur.php
+++ acteur.php
@@ -36,45 +36,47 @@
             <div class="acteur_commentaires__header_com">
                 <?php 
 
-                $nb_post = $bdd->query('SELECT post FROM post WHERE id_acteur = '.$_GET['id'].'')->fetchAll();?>
-                <h1><?php echo count($nb_post) ?> Commentaires</h1>
+                $nb_post = $bdd->query('SELECT post FROM post WHERE id_acteur = '.$_GET['id'].'')->fetchAll(); // stock la liste des posts relatif à l'acteur ?> 
+                <h1><?php echo count($nb_post) ?> Commentaires</h1> <!-- affiche le nombre de posts -->
 
                 <form class="acteur_commentaires__header_com__vote" method="post">
                     <?php 
-                        $countLikes = $bdd->query('SELECT vote FROM vote WHERE vote = 1 AND id_acteur = '.$_GET['id'].'')->fetchAll();
-                        $countDislikes = $bdd->query('SELECT vote FROM vote WHERE vote = 0 AND id_acteur = '.$_GET['id'].'')->fetchAll();
+                        $countLikes = $bdd->query('SELECT vote FROM vote WHERE vote = 1 AND id_acteur = '.$_GET['id'].'')->fetchAll(); // stock la liste des votes où le vote est true et où l'acteur est égal à l'acteur où je suis 
+                        $countDislikes = $bdd->query('SELECT vote FROM vote WHERE vote = 0 AND id_acteur = '.$_GET['id'].'')->fetchAll(); // stock la liste des votes où le vote est false et où l'acteur est égal à l'acteur où je suis 
                     ?>
                     <input type="submit" class="acteur_commentaires__header_com__like" name="like" value=" ">
-                    <p><?php echo count($countLikes) ?></p>
+                    <p><?php echo count($countLikes) ?></p> <!-- affiche le nombre de likes -->
                     <input type="submit" class="acteur_commentaires__header_com__dislike" name="dislike" value=" ">
-                    <p><?php echo count($countDislikes) ?></p>
+                    <p><?php echo count($countDislikes) ?></p> <!-- affiche le nombre de dislikes -->
                 </form>
 
                     <?php
                         
                         $vote = $bdd->query('SELECT v.* FROM vote as v LEFT JOIN account as a ON v.id_user = a.id_user WHERE a.id_user = '.$_SESSION['id_user'].'')->fetchAll();
+                        // définit l'id_user de la table account comme l'id_user de la table vote
                         $alreadyVoted = $bdd->query('SELECT id_user FROM vote WHERE EXISTS(SELECT id_user FROM vote WHERE id_user='.$_SESSION['id_user'].' AND id_acteur='.$_GET['id'].')')->fetchAll();
+                        // définit une variable qui vérifie si l'id_user et l'id_acteur existent dans la table vote
                         
                         
-                        $like = $bdd->prepare('INSERT INTO vote (id_user, id_acteur, vote) VALUES (?,?,?)');
-                        if (!empty($_POST['like']) && isset($_POST['like'])) {
-                            if (empty($alreadyVoted)) {
-                                header('Location: '.$_SERVER[REQUEST_URI].'');      
-                                $test = $like->execute([$_SESSION['id_user'], $_GET['id'], 1]);
-                            } else if (!empty($alreadyVoted)) {
-                                header('Location: '.$_SERVER[REQUEST_URI].''); 
-                                $deletelike = $bdd->query('DELETE FROM vote WHERE id_user='.$_SESSION['id_user'].' AND id_acteur='.$_GET['id'].'');
-                                exec($deletelike);
+                        $like = $bdd->prepare('INSERT INTO vote (id_user, id_acteur, vote) VALUES (?,?,?)');    // $like = insère les valeurs dans la table vote
+                        if (!empty($_POST['like']) && isset($_POST['like'])) {                                  // si le champ like est rempli (cliqué)
+                            if (empty($alreadyVoted)) {                                                         // si l'utilisateur n'a pas déjà voté
+                                header('Location: '.$_SERVER[REQUEST_URI].'');                                  // recharge la page
+                                $test = $like->execute([$_SESSION['id_user'], $_GET['id'], 1]);                 // execute la requete $like et entre les informations affichées
+                            } else if (!empty($alreadyVoted)) {                                                 // sinon si l'utilisateur a déjà voté
+                                header('Location: '.$_SERVER[REQUEST_URI].'');                                  //recharge la page
+                                $deletelike = $bdd->query('DELETE FROM vote WHERE id_user='.$_SESSION['id_user'].' AND id_acteur='.$_GET['id'].''); 
+                                exec($deletelike);                                                              // supprime le like
                             }
                                 
-                        } else if (!empty($_POST['dislike']) && isset($_POST['dislike'])) {
-                            if (empty($alreadyVoted)) {
-                                header('Location: '.$_SERVER[REQUEST_URI].'');      
-                                $test = $like->execute([$_SESSION['id_user'], $_GET['id'], 0]);
-                            } else if (!empty($alreadyVoted)) {
-                                header('Location: '.$_SERVER[REQUEST_URI].''); 
+                        } else if (!empty($_POST['dislike']) && isset($_POST['dislike'])) {                     // sinon si le champ dislike est rempli (cliqué)
+                            if (empty($alreadyVoted)) {                                                         // si l'utilisateur n'a pas déjà voté
+                                header('Location: '.$_SERVER[REQUEST_URI].'');                                  // recharge la page
+                                $test = $like->execute([$_SESSION['id_user'], $_GET['id'], 0]);                 // execute la requete $like et entre les informations affichées
+                            } else if (!empty($alreadyVoted)) {                                                 // sinon si l'utilisateur a déjà voté
+                                header('Location: '.$_SERVER[REQUEST_URI].'');                                  // recharge la page
                                 $deletelike = $bdd->query('DELETE FROM vote WHERE id_user='.$_SESSION['id_user'].' AND id_acteur='.$_GET['id'].'');
-                                exec($deletelike);
+                                exec($deletelike);                                                              // supprime le like
                             }
                         }
                         
@@ -85,10 +87,10 @@
             </div>
 
             <?php
-                if (isset($_POST['commentaire']) && !empty($_POST['commentaire'])) {
+                if (isset($_POST['commentaire']) && !empty($_POST['commentaire'])) {                        // si le champ commentaire est rempli
 
-                    $create = $bdd->prepare('INSERT INTO post(post, id_user,id_acteur) VALUES(?,?,?)');
-                    $create->execute(array($_POST['commentaire'], $_SESSION['id_user'],$_GET['id']));
+                    $create = $bdd->prepare('INSERT INTO post(post, id_user,id_acteur) VALUES(?,?,?)');     // prepare un insert
+                    $create->execute(array($_POST['commentaire'], $_SESSION['id_user'],$_GET['id']));       // insert dans la bdd le commentaire avec l'id de l'utilisateur et de l'acteur
                     header('Location: '.$_SERVER[REQUEST_URI].'');
                 }
             ?>
@@ -97,14 +99,14 @@
         
                 <?php       
 
-                    $appel = $bdd->query('SELECT * FROM post INNER JOIN account ON post.id_user = account.id_user');
+                    $appel = $bdd->query('SELECT * FROM post INNER JOIN account ON post.id_user = account.id_user');    // définit l'ud_user de account en tant que id_user de post
                     $appel->execute();
-                    while ($liste = $appel->fetch()) {
+                    while ($liste = $appel->fetch()) { // tant qu'on récupère les lignes de $appel
 
-                        if ($liste['id_acteur'] == $_GET['id']) {
+                        if ($liste['id_acteur'] == $_GET['id']) {   // si l'id_acteur de post est égal a l'acteur affiché
 
                             ?><div class="acteur_commentaires__commentaires"><div class="acteur_commentaires__nom"><?php echo $liste['prenom'] . " " . $liste['nom'] ?> </div><br><div class="acteur_commentaires__post"><?php echo $liste['post']; ?></div><br> <?php echo $liste ['date_add'] ?> </div> <br><br><?php
-
+                            // affiche les commentaires
                         }
                
                     } 
