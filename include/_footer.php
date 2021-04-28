b7b8240e037a239494359da827af1cafb7cf7bf5
Maxime Lefebvre <lefebvremaxime00@gmail.com>
2021-04-28 14:21:26 +0200

    optimized the css (deleted the useless code and added variables for the colors)

--- include/_footer.php
+++ include/_footer.php
@@ -1,4 +1,4 @@
     <footer>
-        <a href="mentions_legales.html"><p>Mentions légales</p></a>
-        <a href="contact.html"><p>Contact</p></a>
+        <a href="#">Mentions légales</a>
+        <a href="#">Contact</a>
     </footer>
